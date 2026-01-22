75eca43625d4a399689f0a645cecb02ac00e4455
Maggy30ev <maggy30ev@gmail.com>
2026-01-22 01:49:55 -0600

    Inicialización del proyecto

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from proc-dig-img!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ paths.py
@@ -0,0 +1,43 @@
+"""Modulo para la gestion de rutas en el proyecto."""
+
+from pyprojroot import here
+from pathlib import Path
+from typing import (
+    Union,
+    Callable,
+    Iterable,
+)
+
+
+def make_dir_function(dir_name: Union[str, Iterable[str]]) -> Callable[..., Path]:
+    """Función que convierte un string o iterable de strings 
+    en una ruta relativa del proyecto.
+
+    Args:
+        dirname: Nombre de los subdirectorios para extender la ruta del proyecto principal.
+            Si se pasa un iterable de strings como argumento, entonces se colapsa en
+            una sola cadena con anclajes dependientes del sistema operativo.
+
+    Returns:
+        Una función que devuelve la ruta relativa a un directorio que puede
+        recibir `n` número de argumentos para expansión.
+    """
+
+    def dir_path(*args) -> Path:
+        if isinstance(dir_name, str):
+            return here().joinpath(dir_name, *args)
+        else:
+            return here().joinpath(*dir_name, *args)
+
+    return dir_path
+
+
+"""Definición del directorio raíz del proyecto y subdirectorios importantes."""
+project_dir = make_dir_function("")
+
+for dir_type in [
+    ["data"],
+    ["fase_1_aumento_de_datos"],
+]:
+    dir_var = "_".join(dir_type) + "_dir"
+    exec(f"{dir_var} = make_dir_function({dir_type})")
